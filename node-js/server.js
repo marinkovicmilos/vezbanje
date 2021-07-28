baa6aacadf7661cce5b344954c3b10a443b0bd35
marinkovicmilos <39673946+marinkovicmilos@users.noreply.github.com>
2021-07-28 12:44:12 +0200

    Use images mongo and mongo-express from Docker Hub. Updated README.md (#2)
    Co-authored-by: Milos Marinkovic <milos.marinkovic@sap.com>

--- node-js/server.js
+++ node-js/server.js
@@ -3,39 +3,70 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const { ObjectId } = require('mongodb');
 
-const connectionString = 'mongodb+srv://marinkomil:marinkopfc@cluster-somi-0.jboip.mongodb.net/quotesdb?retryWrites=true&w=majority';
+// mongo atlas connection string
+// const connectionString = 'mongodb+srv://marinkomil:marinkopfc@cluster-somi-0.jboip.mongodb.net/quotesdb?retryWrites=true&w=majority';
+
+// mongo db connection string
+const connectionString = 'mongodb://admin:password@localhost:27017';
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.listen(3000, function() {
   console.log('listening on 3000');
 });
 
-MongoClient.connect(connectionString)
-  .then(client => {
-    console.log('Connected to Database');
-    const db = client.db('quotesdb');
-    const quotesCollection = db.collection('quotes');
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.get('/', (req, res) => {
-      db.collection('quotes').find().toArray()
-        .then(results => {
-          console.log('results: :' + results);
-          res.send(results);
-        })
-        .catch(err => console.error('ERROR: ' + err));
-    });
-    app.post('/quotes', (req, res) => {
-      quotesCollection.insertOne(req.body)
+// otvarati konekciju pri svakom pozivu i zatvarati po zavrsetku
+app.get('/', (req, res) => {
+  MongoClient.connect(connectionString)
+    .then(client => {
+      client.db('somi-crud-db').collection('quotes').find().toArray()
         .then(result => {
-          console.log('result: ' + result);
+          console.log('Result: ', JSON.stringify(result));
+          res.send(result);
+        })
+        .catch(error => {
+          console.error('PUCE GRESKA: ' + error);
+        })
+        .finally(() => {
+          client.close();
+        });
+    })
+    .catch(error => {
+      console.error('PUCE GRESKA: ' + error);
+      res.send('Error occured!')
+    });
+});
+
+app.post('/quotes', (req, res) => {
+  MongoClient.connect(connectionString)
+    .then(client => {
+      client.db('somi-crud-db').collection('quotes').insertOne(req.body)
+        .then(result => {
+          console.log('result: ' + JSON.stringify(result));
           res.send('created');
         })
-        .catch(err => console.error('ERROR: ' + err));
+        .catch(err => {
+          console.error('PUCE GRESKA: ' + err);
+          res.send('Error occured!')
+        })
+        .finally(() => {
+          client.close();
+        });
+    })
+    .catch(error => {
+      console.error('PUCE GRESKA: ' + error);
+      res.send('Error occured!')
     });
-    app.put('/quotes', (req, res) => {
+});
+
+app.put('/quotes', (req, res) => {
+  MongoClient.connect(connectionString)
+    .then(client => {
       console.log(req.body);
-      quotesCollection.findOneAndUpdate(
+      client.db('somi-crud-db').collection('quotes').findOneAndUpdate(
         { _id: new ObjectId(req.body._id) },
         {
           $set: {
@@ -45,21 +76,99 @@ MongoClient.connect(connectionString)
         }
       )
       .then(result => {
-        console.log('result: ' + result);
+        console.log('result: ' + JSON.stringify(result));
         res.send('updated');
       })
-      .catch(err => console.error('ERROR: ' + err));
+      .catch(err => {
+        console.error('PUCE GRESKA: ' + err);
+        res.send('Error occured!')
+      })
+      .finally(() => {
+        client.close();
+      });
+    })
+    .catch(error => {
+      console.error('PUCE GRESKA: ' + error);
+      res.send('Error occured!')
     });
-    app.delete('/quotes', (req, res) => {
+});
+
+app.delete('/quotes', (req, res) => {
+  MongoClient.connect(connectionString)
+    .then(client => {
       console.log(req.body);
-      quotesCollection.deleteOne(
+      client.db('somi-crud-db').collection('quotes').deleteOne(
         { _id: new ObjectId(req.body._id) }
       )
       .then(result => {
-        console.log('result: ' + result);
+        console.log('result: ' + JSON.stringify(result));
         res.send('deleted');
       })
-      .catch(err => console.error('ERROR: ' + err));
+      .catch(err => {
+        console.error('PUCE GRESKA: ' + err);
+        res.send('Error occured!')
+      })
+      .finally(() => {
+        client.close();
+      });
+    })
+    .catch(error => {
+      console.error('PUCE GRESKA: ' + error);
+      res.send('Error occured!')
     });
-  })
-  .catch(err => console.error('ERROR: :' + err));
+});
+
+// otvoriti konekciju jednom i ne zatvarati za sve CRUD operacije
+// MongoClient.connect(connectionString)
+//   .then(client => {
+//     console.log('Connected to Database');
+//     const db = client.db('somi-crud-db');
+//     const quotesCollection = db.collection('quotes');
+//     app.use(bodyParser.json());
+//     app.use(bodyParser.urlencoded({ extended: true }));
+//     app.get('/', (req, res) => {
+//       db.collection('quotes').find().toArray()
+//         .then(results => {
+//           console.log('results: :' + JSON.stringify(results));
+//           res.send(results);
+//         })
+//         .catch(err => console.error('PUCE GRESKA: ' + err));
+//     });
+//     app.post('/quotes', (req, res) => {
+//       quotesCollection.insertOne(req.body)
+//         .then(result => {
+//           console.log('result: ' + JSON.stringify(result));
+//           res.send('created');
+//         })
+//         .catch(err => console.error('PUCE GRESKA: ' + err));
+//     });
+//     app.put('/quotes', (req, res) => {
+//       console.log(req.body);
+//       quotesCollection.findOneAndUpdate(
+//         { _id: new ObjectId(req.body._id) },
+//         {
+//           $set: {
+//             name: req.body.name,
+//             quote: req.body.quote
+//           }
+//         }
+//       )
+//       .then(result => {
+//         console.log('result: ' + JSON.stringify(result));
+//         res.send('updated');
+//       })
+//       .catch(err => console.error('PUCE GRESKA: ' + err));
+//     });
+//     app.delete('/quotes', (req, res) => {
+//       console.log(req.body);
+//       quotesCollection.deleteOne(
+//         { _id: new ObjectId(req.body._id) }
+//       )
+//       .then(result => {
+//         console.log('result: ' + JSON.stringify(result));
+//         res.send('deleted');
+//       })
+//       .catch(err => console.error('PUCE GRESKA: ' + err));
+//     });
+//   })
+//   .catch(err => console.error('PUCE GRESKA: :' + err));
